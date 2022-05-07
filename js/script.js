29f1fc610200f26400dedfb383df2b9b9d2d4295
Mihail <mshestak4246633@gmail.com>
2022-05-07 19:29:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let category = 'toys';
+
+console.log(`http://someurl.com/${category}`);
+
+let user = 'Ivan';
+
+alert(`hello, ${user}`);
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+for (i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+let isChecked = true,
+    isClosed = true;
+
+console.log(isChecked || isClosed);
+
+console.log(2 + 2 * 6 !== '6');
