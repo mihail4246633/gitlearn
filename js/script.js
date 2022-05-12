336220ee3b0ba4ce7d5c0ecb90299ad2fa9c65d3
Mihail <mshestak4246633@gmail.com>
2022-05-12 21:30:53 +0300

    added new script

--- js/script.js
+++ js/script.js
@@ -1,24 +1,45 @@
-let category = 'toys';
+// let category = 'toys';
 
-console.log(`http://someurl.com/${category}`);
+// console.log(`http://someurl.com/${category}`);
 
-let user = 'Ivan';
+// let user = 'Ivan';
 
-alert(`hello, ${user}`);
+// alert(`hello, ${user}`);
 
-let incr = 10,
-    decr = 10;
+// let incr = 10,
+//     decr = 10;
 
-console.log(incr++);
-console.log(decr--);
+// console.log(incr++);
+// console.log(decr--);
 
-for (i = 0; i < 10; i++) {
-    console.log(i);
-}
+// for (i = 0; i < 10; i++) {
+//     console.log(i);
+// }
 
-let isChecked = true,
-    isClosed = true;
+// let isChecked = true,
+//     isClosed = true;
 
-console.log(isChecked || isClosed);
+// console.log(isChecked || isClosed);
 
-console.log(2 + 2 * 6 !== '6');
+// console.log(2 + 2 * 6 !== '6');
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+let qw1 = prompt('Один из последних просмотренных фильмов?');
+let qw2 = +prompt('На сколько оцените его?');
+personalMovieDB.movies[qw1] = qw2;
+
+let qw3 = prompt('Один из последних просмотренных фильмов?');
+let qw4 = +prompt('На сколько оцените его?');
+personalMovieDB.movies[qw3] = qw4;
+
+console.log(personalMovieDB);
